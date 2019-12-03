960e46c21b89cd96aeb1ba3306c5697148a4d066
Todd Walraven <cod4247@yahoo.com>
2019-12-02 16:04:23 -0800

    added subtract HTML form and javasript

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,36 @@
+// Business (or back-end) logic:
+
 var add = function(number1, number2) {
-  return number1 + number2
+  return number1 + number2;
 };
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
-alert(add(number1, number2));
 
 var subtract = function(number1, number2) {
   return number1 - number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
-alert(subtract(number1, number2));
-
 var multiply = function(number1, number2) {
   return number1 * number2;
 };
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
-alert(multiply(number1, number2));
 
 var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+// Everything below this line is user interface (or front-end) logic:
 
-alert(divide(number1, number2));
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output-add").text(result);
 
-
-
-var height = parseInt(prompt("what is your height in inches?"))
-var weight = parseInt(prompt("what is your weight in lbs?"))
-
-var result = function(whatToSay) {
-return  weight / height
-}
-alert(result)
+    $("form#subtract").submit(function(event) {
+      event.preventDefault();
+      var number1 = parseInt($("#subtract1").val());
+      var number2 = parseInt($("#subtract2").val());
+      var result = subtract(number1, number2);
+      $("#output-add").text(result);
+  });
+});
